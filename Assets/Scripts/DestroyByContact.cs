c7d10b9aaf7f63b900f7a3964ad22dd56fbb22f5
cmercier <clemmercier@hotmail.com>
2014-05-06 21:09:43 +0200

    beginning of Attrape Note

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public int scoreValue;
+	private GameControllerAtrappeNotes gameController;
+	
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameControllerAtrappeNotes");
+		if (gameControllerObject != null)
+			gameController = gameControllerObject.GetComponent <GameControllerAtrappeNotes> ();
+		if (gameControllerObject == null)
+			Debug.Log ("Cannot find GameController script");
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary")
+			return;
+
+		/*if (other.tag == "Note")
+		{
+
+		}*/
+
+		gameController.AddScore (scoreValue);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/GameControllerAtrappeNotes.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerAtrappeNotes : MonoBehaviour {
+
+	public GameObject note;
+	public Vector3 spawnValues;
+	public int noteCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	
+	public GUIText scoreText;
+	public GUIText restartText;
+	
+	private int score;
+	
+	void Start()
+	{
+		restartText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (spawnWaves());
+	}
+	
+	void Update()
+	{
+		if (score == 50)
+			restartText.text = "Bravo vous avez gagné";
+	}
+	
+	IEnumerator spawnWaves()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < noteCount; i++) 
+			{
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x) , spawnValues.y, spawnValues.z);
+				Instantiate(note, spawnPosition, Quaternion.identity);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+		
+	}
+	
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+}
