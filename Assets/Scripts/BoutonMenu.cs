365106afcce06dd15cde169ceff8122f03023220
Nicolas Stempak <nicolas.stempak@gmail.com>
2014-05-06 10:21:25 +0200

    Basic main menu + button.

---
+++ Assets/Scripts/BoutonMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoutonMenu : MonoBehaviour {
+	
+	void OnMouseEnter() {
+		renderer.enabled = true;
+	}
+
+	void OnMouseExit() {
+			renderer.enabled = false;
+	}
+	
+}
