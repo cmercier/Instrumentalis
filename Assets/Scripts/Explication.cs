0594e80ec80eb4da0714e48ecb051a0ee819e07e
Nicolas Stempak <nicolas.stempak@gmail.com>
2014-05-28 12:04:26 +0200

    Add guide, mise en prefab

---
+++ Assets/Scripts/Explication.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explication : MonoBehaviour {
+
+	public GameObject parent;
+	void OnMouseDown()
+	{
+				parent.SetActive(false);
+	}
+}
